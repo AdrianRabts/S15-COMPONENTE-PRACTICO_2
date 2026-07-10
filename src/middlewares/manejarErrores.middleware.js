edfab1ad0eec7f50a2fc6949db9c809664ad39f8
Joseph Mateo <josephmateomb@hotmail.com>
2026-07-09 19:28:54 -0500

    feat: agregar middleware centralizado de manejo de errores

---
+++ src/middlewares/manejarErrores.middleware.js
@@ -0,0 +1,13 @@
+function manejarErrores(err, req, res, next) {
+  console.error(err.stack || err);
+  res.status(500).json({ error: 'Error interno del servidor' });
+}
+
+function rutaNoEncontrada(req, res) {
+  res.status(404).json({ error: 'Ruta no encontrada' });
+}
+
+module.exports = {
+  manejarErrores,
+  rutaNoEncontrada,
+};
