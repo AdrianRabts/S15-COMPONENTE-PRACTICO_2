505f8c2de2713fae99e5ec1a666981a856f97773
Joseph Mateo <josephmateomb@hotmail.com>
2026-07-09 19:24:51 -0500

    feat: agregar ruta PATCH /api/tareas/:id/completar

--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const tareasController = require('../controllers/tareas.controller');
-const { validarCrearTarea } = require('../middlewares/validar.middleware');
+const { validarCrearTarea, validarId } = require('../middlewares/validar.middleware');
 
 const router = express.Router();
 
 router.get('/', tareasController.listar);
 router.post('/', validarCrearTarea, tareasController.crear);
+router.patch('/:id/completar', validarId, tareasController.completar);
 
 module.exports = router;
