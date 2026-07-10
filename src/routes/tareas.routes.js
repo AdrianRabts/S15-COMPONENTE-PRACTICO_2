34b89ea3fa003a60fed69a3a336081b30544de0f
Joseph Mateo <josephmateomb@hotmail.com>
2026-07-09 19:25:20 -0500

    feat: agregar ruta DELETE /api/tareas/:id

--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/', tareasController.listar);
 router.post('/', validarCrearTarea, tareasController.crear);
 router.patch('/:id/completar', validarId, tareasController.completar);
+router.delete('/:id', validarId, tareasController.eliminar);
 
 module.exports = router;
