4cf3114924b36a4b7faa64eef831165760d5f1a6
Joseph Mateo <josephmateomb@hotmail.com>
2026-07-09 19:41:51 -0500

    docs: documentar por qué manejarErrores requiere 4 parámetros

--- src/middlewares/manejarErrores.middleware.js
+++ src/middlewares/manejarErrores.middleware.js
@@ -1,3 +1,4 @@
+// Express reconoce este middleware como manejador de errores por tener 4 parámetros (err, req, res, next).
 function manejarErrores(err, req, res, next) {
   console.error(err.stack || err);
   res.status(500).json({ error: 'Error interno del servidor' });
